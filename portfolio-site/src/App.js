b9a82ff5111717f606e7cd5236b60ddbdffc44fd
Bob <bkane90@gmail.com>
2021-01-17 23:15:43 -0500

    router

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
 
   constructor(props) {
     super(props);
@@ -28,7 +29,10 @@ class App extends React.Component() {
 
   render() {
     return (
-      <div>Hello from React</div>
+      <Router>
+
+
+      </Router>
     );
   }
 }
