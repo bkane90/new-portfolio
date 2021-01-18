2eca5dc85a0b8f098f4c1f2839d3aa30503d40b5
Bob <bkane90@gmail.com>
2021-01-18 15:08:21 -0500

    navbar

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
@@ -30,7 +33,21 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+        <Container className="p-0" fluid={true}>
 
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Robert Kane, Jr.</Navbar.Brand>
+            <Navbar.Toggle aria-controls="hamburger-menu" />
+            <Navbar.Collapse id="hamburger-menu">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
       </Router>
     );
   }
