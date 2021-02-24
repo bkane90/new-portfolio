2fdeb0e8098a7b0f39956d3816e12669fd578863
Bob <bkane90@gmail.com>
2021-02-23 22:16:14 -0500

    adjust font and format

--- portfolio-site/src/pages/AboutPage.js
+++ portfolio-site/src/pages/AboutPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
 
-import MyPhoto from '../assets/images/MyPhoto.png';
+// import MyPhoto from '../assets/images/MyPhoto.png';
 
 function AboutPage(props) {
 
