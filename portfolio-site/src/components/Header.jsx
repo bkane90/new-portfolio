265308846718deb9cb61fcd161247202817a9d5f
Bob <bkane90@gmail.com>
2021-02-27 17:52:11 -0500

    create header component

---
+++ portfolio-site/src/components/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
+import ContactPage from '../pages/ContactPage';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Robert Kane, Jr.',
+            headerLinks: [
+                { title: 'Home', path: '/' },
+                { title: 'About', path: '/about' },
+                { title: 'Contact', path: '/contact' },
+            ],
+            home: {
+                title: 'Be Extraordinary',
+                subTitle: 'Raise Your Standards',
+                text: 'Check out my projects below'
+            },
+            about: {
+                title: 'About Me',
+            },
+            contact: {
+                title: 'Let\'s Talk',
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Container className="p-0 color:white" fluid={true}>
+
+                    <Navbar className="border-bottom" bg="light" variant="light" expand="lg" sticky="top" >
+                        <Navbar.Brand>Robert Kane, Jr.</Navbar.Brand>
+                        <Navbar.Toggle className="border-0" aria-controls="hamburger-menu" />
+                        <Navbar.Collapse id="hamburger-menu">
+                            <Nav className="ml-auto">
+                                <Link className="nav-link" to="/">Home</Link>
+                                <Link className="nav-link" to="/about">About</Link>
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+
+                    <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+                    <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+                    <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+
+                </Container>
+            </Router>
+        )
+    }
+}
+
+export default Header;
--- portfolio-site/src/pages/AboutPage.jsx
+++ portfolio-site/src/pages/AboutPage.jsx
@@ -17,7 +17,7 @@ function AboutPage(props) {
 
                 <p>Critical thinking and problem solving are in my nature, so it's fitting that I can fully integrate those skills into my career through coding. I'm very solutions-oriented, and I instinctively try to understand why and how things work the way they do. </p>
 
-                <p> As a student of life, I'm deeply passionate about learning and self-improvement. In my constant quest for knowledge, I'm always seeking to better myself and continually learn new things in all facets of life. In terms of web development, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
+                <p> As a student of life, I'm deeply passionate about learning and self-improvement. In my neverending quest for knowledge, I'm always seeking to better myself and continually learn new things in all facets of life. In terms of web development, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
 
                 <p>When I'm not busy coding, you can usually find me enjoying some of my favorite hobbies. These include meditation, yoga, weight-training, snowboarding, DJing, cooking, fishing, and traveling. Besides programming languages, I'm also currently learning how to speak Spanish. Eventually I strive to be bilingual and add that to my dynamic skillset.</p>
             </Content>
