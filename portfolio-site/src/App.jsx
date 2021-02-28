da731e4e0f4a76123f73a5caf13fc212e8ae525f
Bob <bkane90@gmail.com>
2021-02-28 16:42:07 -0500

    add bg, edit text

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import background from './assets/images/BackgroundImage2.jpg'
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${background})`, height: 'auto', width: '100%' }}>
       <Header />
       <Footer />
     </div>
