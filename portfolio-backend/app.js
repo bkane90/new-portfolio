79254c071ae6e7bc3bcb3b72f329c21dfa6763a9
Bob <bkane90@gmail.com>
2021-03-16 14:27:52 -0400

    ready to deploy

--- portfolio-backend/app.js
+++ portfolio-backend/app.js
@@ -46,8 +46,8 @@ app.post('/api/email', (req, res, next) => {
         });
 });
 
-app.listen(3030, '0.0.0.0');
+// app.listen(3030, '0.0.0.0');
 
-// app.listen(process.env.PORT, () => {
-//     console.log('Listening!');
-// })
+app.listen(process.env.PORT, () => {
+    console.log('Listening!');
+})
