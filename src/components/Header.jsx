cdc4abbc98012da097b41b083d1ee8d1c921db7c
Bob <bkane90@gmail.com>
2021-03-19 11:22:24 -0400

    adjust hero and card sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ class Header extends React.Component {
                 { title: 'Contact', path: '/contact' },
             ],
             home: {
-                title: 'Dare to be Different',
+                title: 'DARE TO BE DIFFERENT',
                 subTitle: 'Strive for Excellence',
                 text: 'Check out my projects below'
             },
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,10 @@ function Hero(props) {
         <Jumbotron className="bg-transparent jumbotron-fluid p-0 text-center">
             <Container fluid={true}>
                 <Row className="justify-content-center pt-5 pb-4">
-                    <Col md={8} sm={12}>
-                        {props.title && <h1 className="display-2 font-weight-bolder">{props.title}</h1>}
+                    <Col md={8} sm={12} className="p-0">
+                        {props.title && <h1 className="display-3 font-weight-bolder">{props.title}</h1>}
                         {props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3>}
-                        {props.text && <h3 className="lead font-weight-light">{props.text}</h3>}
+                        {props.text && <h3 className="font-weight-light">{props.text}</h3>}
                     </Col>
 
                 </Row>
