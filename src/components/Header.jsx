7f6139a63626e1eb017cf3637f337208b450a6c8
Bob <bkane90@gmail.com>
2021-03-23 11:30:26 -0400

    unmerge header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,6 @@ class Header extends React.Component {
         return (
             <Router>
                 <Container className="p-0 color:white" fluid={true}>
-
                     <Navbar className="border-bottom" bg="light" variant="light" expand="lg" sticky="top" >
                         <Navbar.Brand>Robert Kane, Jr.</Navbar.Brand>
                         <Navbar.Toggle className="border-0" aria-controls="hamburger-menu" />
