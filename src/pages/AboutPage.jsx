5be91416e0451e36d88ea0e0b0fb88283cd64ff9
Bob <bkane90@gmail.com>
2021-07-01 15:19:24 -0400

    edit formatting and text

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -15,7 +15,7 @@ function AboutPage(props) {
                     <Content>
                         <p>Hey there! I'm Bob Kane, a full-stack web developer based out of Brooklyn, NY with a background in finance and marketing.
                             I'm currently working as a freelance web developer while I pursue my next full-time role. As an Ironhack bootcamp graduate,
-                            I'm most familar with the technologies used in the MERN stack (MongoDB, Express, React, Node.js) as well as JavaScript, HTML, CSS, and Wordpress. </p>
+                            I'm most familar with the technologies used in the MERN stack (MongoDB, Express, React, Node.js) as well as JavaScript, HTML, CSS, and WordPress. </p>
 
                         <p>Critical thinking and problem solving are in my nature, so it's fitting that I can fully integrate those skills into my career through coding.
                             I'm very solutions-oriented, and I instinctively try to understand why and how things work the way they do.
@@ -25,9 +25,9 @@ function AboutPage(props) {
                             In my neverending quest for knowledge, I'm always seeking to better myself and continually learn new things in all aspects of life.
                             In terms of web development, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
 
-                        <p>Besides programming and self-improvement, I'm also immensely dedicated to health, fitness, and mindfulness.
+                        <p>Other than coding and self-improvement, I'm also immensely dedicated to health, fitness, and mindfulness.
                             When I'm not busy coding, you can usually find me enjoying some of my favorite hobbies.
-                            These include reading, meditation, yoga, exercise, snowboarding, DJing, fishing, cooking, traveling, and solving jigsaw puzzles.
+                            These include reading, meditation, yoga, exercising, snowboarding, DJing, fishing, cooking, traveling, and solving jigsaw puzzles.
                             Besides programming languages, I'm also currently learning how to speak Spanish. Eventually I strive to be bilingual and add that to my already dynamic skillset.</p>
                     </Content>
                 </Row>
