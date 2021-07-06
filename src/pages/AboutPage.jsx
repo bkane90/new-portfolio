23b827e7b07184ccb8cc2a00476c7930300b2007
Bob <bkane90@gmail.com>
2021-07-06 17:05:49 -0400

    edit about page and project borders

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -25,9 +25,8 @@ function AboutPage(props) {
                             In my neverending quest for knowledge, I'm always seeking to better myself and continually learn new things in all aspects of life.
                             In terms of web development, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
 
-                        <p>Other than coding and self-improvement, I'm also immensely dedicated to health, fitness, and mindfulness.
-                            When I'm not busy coding, you can usually find me enjoying some of my favorite hobbies.
-                            These include reading, meditation, yoga, exercising, snowboarding, DJing, fishing, cooking, traveling, and solving jigsaw puzzles.
+                        <p> When I'm not busy coding, you can usually find me enjoying some of my favorite hobbies.
+                            These include reading, meditation, yoga, exercising, snowboarding, DJing, fishing, cooking, and traveling.
                             Besides programming languages, I'm also currently learning how to speak Spanish. Eventually I strive to be bilingual and add that to my already dynamic skillset.</p>
                     </Content>
                 </Row>
